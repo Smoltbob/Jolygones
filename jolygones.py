6970f436de8f337ab3572aadf796659f9d858a6b
Jules Simon <jules.simon@grenoble-inp.org>
2017-07-25 11:56:50 +0200

    english traduction

--- jolygones.py
+++ jolygones.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 """
-Génère des jolygones
-Paramètres : Un angle et un coefficient multiplicateur
-A chaque itération, on trace une nouvelle ligne selon l'angle donné
+Generates Jolygones.
+To make one we start wih a line segment. Each new segment is obtained
+from the last one, by adding a rotation. (see examples)
+Parameters : an angle and a coefficient
+At each iteration, we trace a new line according to the given angle.
 """
 
 # TODO : meilleure gestion du min max : ne pas faire de seconde itération
 # TODO : couleur
+# TODO : random angles
+# TODO : no preprocessing
+# TODO : use functions
+# TODO : write header at the begining of main loop
+# TODO : add if name == main..
 
 from math import cos, sin, pi, fabs
 import svg
 import argparse
 
-def arc_en_ciel(frequence, index):
+def rainbow(frequence, index):
     """
-    Retourne un arc-en-ceil de couleurs
+    Returns the RGB values to make a rainbow
     """
     rouge = sin(frequence * index + 0) * 127 + 128
     vert = sin(frequence * index + 2) * 127 + 128
@@ -23,33 +30,36 @@ def arc_en_ciel(frequence, index):
 
 def main():
     """
-    Fonction principale
+    Main loop.
+    Handles arguments and computes the Jolygone
     """
-    # Parseur d'arguments
-    parser = argparse.ArgumentParser(description = "Génère des Joligones")
-    parser.add_argument("-angle", "-a", help = "Angle des segments", type = float)
-    parser.add_argument("-coefficient", "-c", help = "Coefficient d'atténuation", type = float)
-    parser.add_argument("-iterations", "-i", help = "Nombre de segments", type = int)
-    parser.add_argument("-scale", "-s", help = "Taille du jolygone", type = int)
+    # Parsing arguments
+    parser = argparse.ArgumentParser(description = "Generates Jolygones")
+    parser.add_argument("-angle", "-a", help = "Angle of segments rotation", type = float)
+    parser.add_argument("-coefficient", "-c", help = "Coefficient of attenuation", type = float)
+    parser.add_argument("-iterations", "-i", help = "Number of segments", type = int)
+    parser.add_argument("-scale", "-s", help = "Size of Jolygone", type = int)
     args = parser.parse_args()
 
-    # Affectation des paramètres
+    # Affecting parameters
     angle = args.angle * (pi/180)
     coeff = args.coefficient
     iterations = args.iterations
     scale = args.scale
 
-    # Couleurs
+    # Colors
     index = 0
 
-    # Calcul du ymax : on itère une première fois pour trouver les extremum
+    # Computing ymax : we iterate a first time to find extremums.
+    # It is better to compute the extremums directly with a formula.
+    # We will use arrays to store the Jolygone points
     pointa = (0, 0)
     pointb = (scale, 0)
     tabx = []
     taby = []
     ligne = 1
     for _ in range(iterations):
-        # Rotation du point A autour de B selon l'angle
+        # Rotation of point A around point B according to the angle.
         xc = ((pointa[0] - pointb[0]) * coeff * cos(angle)
               - (pointa[1] - pointb[1]) * coeff * sin(angle) + pointb[0])
         yc = ((pointa[0] - pointb[0]) * coeff  * sin(angle)
@@ -59,43 +69,43 @@ def main():
         tabx.append(pointa[0])
         taby.append(pointa[1])
 
-    # Calcul du min et max
+    # Computing the span of the figure
     maxix = max(tabx) - min(tabx)
     maxiy = max(taby) + fabs(min(taby))
-    # Centrage de la figure en choisissant des
-    # coordonnées de départ judicieuses
+
+    # Centering the figure by using appropriate coordinates
     pointa = (- min(tabx), - min(taby))
     pointb = (scale - min(tabx), - min(taby))
 
-    (rouge, vert, bleu) = arc_en_ciel(0.03, index)
+    (rouge, vert, bleu) = rainbow(0.03, index)
+
+    # Generating figure
     with open("test.svg", "w+") as svg_file:
-        # Ecriture de l'en-tête. On laisse de la place pour le header
-        # TODO : trouver une meilleure solution ou calculer la taille
-        # précise du header
+        # We keep some room for the header
         svg_file.write("                                                    ")
         svg_file.write(svg.svg_line(pointa[0], pointa[1],
                                     pointb[0], pointb[1],
                                     rouge, vert, bleu, ligne))
         for boucle in range(iterations):
-            # Mise à jour des couleurs
+            # Updating colors
             index += 1
             if index > 32:
                 index = 0
-            (rouge, vert, bleu) = arc_en_ciel(0.2, index)
-            # Rotation du point A autour de B selon l'angle
+            (rouge, vert, bleu) = rainbow(0.2, index)
+            # Rotating point A around point B according to the angle
             xc = ((pointa[0] - pointb[0]) * coeff * cos(angle)
                   - (pointa[1] - pointb[1]) * coeff * sin(angle) + pointb[0])
             yc = ((pointa[0] - pointb[0]) * coeff  * sin(angle)
                   + (pointa[1] - pointb[1]) * coeff * cos(angle) + pointb[1])
-            # Mise à jour des points
+            # Updating points
             pointa = pointb
             pointb = (xc, yc)
-            # Tracé du point
+            # Tracing line
             ligne = 2/(pow(boucle + 1, 1/3))
             svg_file.write(svg.svg_line(pointa[0], pointa[1],
                                         pointb[0], pointb[1],
                                         rouge, vert, bleu, ligne))
-        # Fin du fichier
+        # End of figure
         svg_file.write(svg.svg_footer())
         svg_file.seek(0)
         svg_file.write(svg.svg_header(round(maxiy, 3), round(maxix, 3)))
